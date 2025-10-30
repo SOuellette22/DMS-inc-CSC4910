6bde0bc2f1fe98791edecc97b940b289da280240
SOuellette22 <ouellettese@merrimack.edu>
2025-10-29 22:13:42 -0400

    feat: added the stuff Max made to the needed spots in the project to allow it to connect to flask

--- src/core/routes.py
+++ src/core/routes.py
@@ -6,3 +6,15 @@ core_bp = Blueprint('core', __name__, template_folder='templates')
 @core_bp.route("/")
 def home():
     return render_template("index.html")
+
+@core_bp.route("/why")
+def why():
+    return render_template("why.html")
+
+@core_bp.route("/info")
+def info():
+    return render_template("info.html")
+
+@core_bp.route("/conversions")
+def conversions():
+    return render_template("conversions.html")
