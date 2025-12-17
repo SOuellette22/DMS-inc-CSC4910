9cd5e5e0db602cc0eab2145afc2e84e0daadac76
SOuellette22 <ouellettese@merrimack.edu>
2025-12-17 13:53:09 -0500

    Fix: fixed the issue with light mode not working correctly

--- src/app.py
+++ src/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from src.api_key import SECRET_KEY
 
 db = SQLAlchemy()
 
 def create_app():
     # create and configure the Flask application
     app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/")
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///testdb.db'
-    app.config["SECRET_KEY"] = 'test' # TODO this is temporary
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///admin.db'
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.permanent_session_lifetime = timedelta(hours=1)
 
     db.init_app(app)
